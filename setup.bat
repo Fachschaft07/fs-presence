246270de269adf2c62332970403d7bfaca596de2
Thomas Maier <tmaier@fs.cs.hm.edu>
2014-09-10 22:47:27 +0200

    Added Android support in setup.bat

--- setup.bat
+++ setup.bat
@@ -22,12 +22,18 @@ echo Import Certificate into Truststore
 echo ===============================
 keytool -import -v -trustcacerts -alias fspresenceserver -file server.cer -keystore client.jks
 
+echo.
+echo Import Certificate into Android Truststore
+echo ===============================
+keytool -importcert -v -trustcacerts -file "server.cer" -alias fspresenceserver -keystore "keystore.bks" -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath "..\misc\bcprov-jdk15on-146.jar" -storetype BKS -storepass "123456"
+
 
 rem Distribute files
 copy db.sqlite ..\FSPresenceServer\db.sqlite
 mkdir ..\FSPresenceServer\ssl
 copy server.jks ..\FSPresenceServer\ssl\server.jks
 copy client.jks ..\FSPresencesClient\client.jks
+copy keystore.bks ..\FSPresence\FSPresence\res\raw\keystore.bks
 
 cd..
 rmdir /S /q temp
